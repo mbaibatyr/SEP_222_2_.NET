19423432e06ead67bb73a2d9b3b14ff92c92a504
байбатыровм <@213-15>
2023-07-14 21:46:24 +0600

    Добавьте файлы проекта.

---
+++ SEP_222_2_.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SEP_222_2_.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your name: ");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Hello, {input}");
+        }
+    }
+}
