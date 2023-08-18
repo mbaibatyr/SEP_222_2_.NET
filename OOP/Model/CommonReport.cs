8520a89d7f03b0fea88d8ce6335c5bf1a34cc10f
mbaibatyr <murat_b@mail.ru>
2023-08-18 21:44:53 +0600

    -

---
+++ OOP/Model/CommonReport.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace OOP.Model
+{
+    public class CommonReport
+    {
+        protected List<ReportData> Data { get; set; }
+        protected string Path { get; set; }
+        public CommonReport(List<ReportData> data, string path)
+        {
+            Path = path;
+            Data = data;
+        }
+
+        public virtual void Export()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        protected void Run()
+        {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo(Path)
+            {
+                UseShellExecute = true
+            };
+            p.Start();
+        }
+
+        protected string[] getArray()
+        {
+            var array = new string[Data.Count];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = $"{Data[i].Id} {Data[i].Name}";
+            }
+            return array;
+        }
+    }
+
+    class CSV : CommonReport
+    {
+        public CSV(List<ReportData> data, string path) 
+            : base(data, path)
+        {
+        }
+        public override void Export()
+        {
+            Console.WriteLine(this.ToString());
+            File.WriteAllLines(Path, getArray());
+            Run();
+        }
+    }
+
+
+    class Excel : CommonReport
+    {
+        public Excel(List<ReportData> data, string path)
+            : base(data, path)
+        {
+            Data.Add(new ReportData { Id = 4, Name = "Shymkent" });
+        }
+        public override void Export()
+        {
+            Console.WriteLine(this.ToString());
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                var ws = wb.AddWorksheet("report");
+                ws.Cell(1, 1).Value = "Id";
+                ws.Cell(1, 2).Value = "Name";
+                ws.Cell(2, 1).InsertData(Data);
+                wb.SaveAs(Path);
+            }
+            Run();
+        }
+    }
+
+
+    class Html : CommonReport
+    {
+        public Html(List<ReportData> data, string path)
+            : base(data, path)
+        {
+        }
+        public override void Export()
+        {
+            Console.WriteLine(this.ToString());
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<html><body><table border='1'>");
+            sb.AppendLine("<tr><td>Id</td><td>Name</td></tr>");
+            foreach (var item in Data)
+            {
+                sb.AppendLine($"<tr><td>{item.Id}</td><td>{item.Name}</td></tr>");
+            }
+
+            sb.AppendLine("</table></body></html>");
+            File.WriteAllText(Path, sb.ToString());
+            Run();
+        }
+    }
+
+    public class ReportData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ OOP/Model/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Model
+{
+    public class MyClass
+    {
+    }
+
+
+    public class Employees
+    {
+        public string status { get; set; }
+        public Data[] data { get; set; }
+        public string message { get; set; }
+    }
+
+    public class Data
+    {
+        public int id { get; set; }
+        public string employee_name { get; set; }
+        public int employee_salary { get; set; }
+        public int employee_age { get; set; }
+        public string profile_image { get; set; }
+    }
+
+
+}
